381626d5747f6da42b0a1f9ce26dbab089d29e2f
huweidong <407475190@qq.com>
2016-09-10 23:40:45 +0800

    Initial Commit

---
+++ collectionViewZeroSpaceCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionViewZeroSpaceCell
+//
+//  Created by echo on 16/9/10.
+//  Copyright © 2016年 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
